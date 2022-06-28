28f05cf0237c6aae48cc5ce9569fcfb480cd1f4d
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2022-06-28 00:09:06 -0300

    Primeira parte do grid pronta

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -3,9 +3,21 @@
     <div class="row">
       <div class="column-12">
         <div class="grid">
-          <div></div>
-          <div></div>
-          <div></div>
+
+          <!-- GRID ITEM -->
+          <div class="grid-item" v-for="item in items">
+
+            <div class="grid-item-image">
+              <img v-bind:src="item.image" alt="">
+            </div>
+
+            <div class="grid-item-title">
+              <span>{{ item.name }}</span>
+            </div>
+
+          </div>
+          <!-- END GRID ITEM -->
+
         </div>
       </div>
     </div>
@@ -14,9 +26,111 @@
 
 <script>
 export default {
-  name: "Grid"
+  name: "Grid",
+  data: function() {
+    return {
+      items: [
+        {
+          id: 0,
+          name: "A-BOMB",
+          image: "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_incredible.jpg"
+        },
+        {
+          id: 1,
+          name: "THOR",
+          image: "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_incredible.jpg"
+        },
+        {
+          id: 2,
+          name: "HULK",
+          image: "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_incredible.jpg"
+        },
+        {
+          id: 3,
+          name: "SPIDER-MAN",
+          image: "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_incredible.jpg"
+        },
+        {
+          id: 4,
+          name: "A-BOMB",
+          image: "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_incredible.jpg"
+        },
+        {
+          id: 5,
+          name: "THOR",
+          image: "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_incredible.jpg"
+        },
+        {
+          id: 6,
+          name: "HULK",
+          image: "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_incredible.jpg"
+        },
+        {
+          id: 7,
+          name: "SPIDER-MAN",
+          image: "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16/standard_incredible.jpg"
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(317px, auto));
+  grid-gap: 4px;
+
+  & > .grid-item {
+    color: var(--white-01);
+
+    transition: all .2s ease-in-out;
+
+    cursor: pointer;
+
+    display: flex;
+    flex-direction: column;
+
+    .grid-item-image {
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .grid-item-title {
+      padding: 20px;
+
+      span {
+        display: block;
+
+        font-family: "Roboto";
+        font-weight: 900;
+        font-size: 30px;
+        line-height: 35px;
+        letter-spacing: 0.2em;
+        text-transform: uppercase;  
+        text-align: center;
+
+        color: var(--white-01);
+      }
+    }
+
+    &:hover {
+      transform: scale(1.2);
+
+      background: var(--white-01);
+
+      padding: 20px;
+
+      &>.grid-item-title {
+
+        span {
+          color: var(--grey-02);
+        }
+      }
+    }
+  }
+}
 </style>
